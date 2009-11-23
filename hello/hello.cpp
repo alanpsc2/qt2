7d080d8d46d898b2a3feefdf9f49aec49a721749
alanpsc2 <alanpsc@sina.com>
2009-11-23 16:57:53 +0800

    test1

---
+++ hello/hello.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QLabel *label = new QLabel("Hello Qt!");
+    label->show();
+//    return app->exec();
+    return app.exec();
+    test1
+}
